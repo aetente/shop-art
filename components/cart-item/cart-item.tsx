96ce5b1a1b34c08077cb962d028491a8a38587f1
Yehoshua Kopirin <aetente@gmail.com>
2023-07-23 12:16:16 +0300

    feat: added some cart page

---
+++ components/cart-item/cart-item.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+
+function CartItem() {
+  return (
+
+    <div className='flex gap-4 justify-between items-center p-4 border-solid border-[1px] border-r-0 border-l-0 border-black'>
+      <div className='flex gap-4'>
+        <div className="relative min-w-[168px] min-h-[168px]">
+          <Image
+            layout='fill'
+            src='/test/test_image1.jpg'
+            alt='item'
+            style={{
+              objectFit: 'cover',
+              // width: '100%',
+              // height: 'auto'
+            }}
+          />
+        </div>
+        <div>
+          <div className='text-4xl'>SVG File</div>
+          <div className="text-red-500">Delete</div>
+        </div>
+      </div>
+
+      <input type='number' defaultValue={2} className='min-w-[48px] w-12 border-solid border-[1px] border-black text-center' />
+
+      <div>
+        $2.00
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,28 +1,41 @@
-import Image from "next/image"
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 function Header() {
+  
+  const router = useRouter()
+
   return (
     <div className='pr-24 pl-24 fixed w-full h-20 bg-slate-200 z-30 left-0'>
       <div className='flex items-center justify-between w-full max-w-7xl m-auto'>
-        <div className="relative w-[128px] h-[64px]">
+        <div className='relative w-[128px] h-[64px] cursor-pointer'
+          onClick={() => {
+            router.push('/')
+          }}
+        >
           <Image
             layout='fill'
-            src="/images/logo.png"
+            src='/images/logo.png'
             alt='logo'
           />
         </div>
         <div className='flex items-center justify-between gap-2'>
-          <div className="relative w-[32px] h-[32px]">
+          <div className='relative w-[32px] h-[32px]'>
             <Image
               layout='fill'
-              src="/icons/search.png"
+              src='/icons/search.png'
               alt='search'
             />
           </div>
-          <div className="relative w-[32px] h-[32px]">
+          <div
+            className='relative w-[32px] h-[32px] cursor-pointer'
+            onClick={() => {
+              router.push('/cart')
+            }}
+          >
             <Image
               layout='fill'
-              src="/icons/cart.png"
+              src='/icons/cart.png'
               alt='cart'
             />
           </div>
