9ab96d2dcbf9e0c27915d90501e9dd3a12e1e83e
Yehoshua Kopirin <aetente@gmail.com>
2023-08-01 12:09:22 +0300

    fix: tring to fix build error; testing strapi

--- requests/categories.ts
+++ requests/categories.ts
@@ -1,12 +1,14 @@
 export async function getCategories() {
   try {
-    const res = await fetch('http://localhost:80/shopart/api/categories&output_format=JSON', {
+    const res = await fetch('http://localhost:1337/api/categories?populate=thumbnail', {
       method: 'GET',
       headers: {
         'Authorization': 'Basic ' + btoa((process.env.NEXT_PUBLIC_PRESTASHOP_KEY as string) + ':'),
         'Content-Type': 'application/json'
-      }
+      },
     })
+    
+    console.log('getCategories', res)
     const data = await res.json()
     console.log('getCategories', data)
   } catch(e) {
