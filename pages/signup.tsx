55b690956466d0ebcfcf2fb4108cd6eb795ff5eb
Yehoshua Kopirin <aetente@gmail.com>
2023-08-15 13:34:06 +0300

    fix: preparing strapi signup

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,31 +1,15 @@
-import { useRouter } from "next/router"
-
 function Login() {
-  
-  const router = useRouter()
-
   return (
     <div className='min-h-screen flex justify-center items-center'>
       <div className="min-h-[360px] max-w-md bg-slate-200 m-auto p-12 flex flex-col justify-center items-center gap-8">
-        <div>LOGIN</div>
+        <div>SIGN UP</div>
         <input placeholder='email' className='text-2xl' />
         <input type='password' placeholder='password' className='text-2xl' />
 
         <div className="flex flex-col justify-center items-center gap-2 w-full">
           <button className='bg-blue-500 text-gray-50 p-4 w-full'>
-            SIGN IN
+            CREATE ACCOUNT
           </button>
-          <div className='text-blue-600 w-full cursor-pointer'>
-            Forgot password?
-          </div>
-          <div
-            onClick={() => {
-              router.push('/signup')
-            }}
-            className='text-blue-600 w-full cursor-pointer'
-          >
-            Create account
-          </div>
         </div>
       </div>
     </div>
