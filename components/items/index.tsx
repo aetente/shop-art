087975e06f0f097104a62a36fd53a59e301b5afd
Yehoshua Kopirin <aetente@gmail.com>
2023-09-03 11:06:14 +0300

    feat: added some mock items list page

--- components/items/index.tsx
+++ components/items/index.tsx
@@ -1,42 +1,42 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
-const categoriesData = [
+const itemsData = [
   {
-    name: 'category',
+    name: 'item',
     img: '/test/test_image1.jpg'
   },
   {
-    name: 'category',
+    name: 'item',
     img: '/test/test_image1.jpg'
   },
   {
-    name: 'category',
+    name: 'item',
     img: '/test/test_image1.jpg'
   },
   {
-    name: 'category',
+    name: 'item',
     img: '/test/test_image1.jpg'
   },
   {
-    name: 'category',
+    name: 'item',
     img: '/test/test_image1.jpg'
   },
 ]
 
-function Categories() {
+function Items() {
 
   const router = useRouter()
 
-  const mapCategories = (c:any, i:number) => {
+  const mapItems = (c: any, i: number) => {
     return (
-      <div key={`category-${i}`} className='grid w-full relative pb-[100%]'>
+      <div key={`item-${i}`} className='grid w-full relative pb-[100%]'>
         <div className='absolute top-0 left-0 bottom-0 right-0'>
           <p>{c.name}</p>
           <div
             className='relative w-full h-full min-w-[168px] min-h-[100%] mt-2 cursor-pointer'
             onClick={() => {
-              router.push('/items')
+              router.push('/item')
             }}
           >
             <Image
@@ -56,10 +56,13 @@ function Categories() {
   }
 
   return (
-    <div className='w-full grid grid-flow-column grid-cols-2 auto-rows-[1fr] auto-cols-[1fr] gap-x-4 gap-y-4'>
-      {categoriesData.map(mapCategories)}
+    <div className='w-full grid grid-flow-column grid-cols-6 auto-rows-[1fr] auto-cols-[1fr] gap-x-4 gap-y-4'>
+      {Array(20).fill({
+        name: 'item',
+        img: '/test/test_image1.jpg'
+      }).map(mapItems)}
     </div >
   )
 }
 
-export default Categories
+export default Items
---
+++ pages/items.tsx
@@ -0,0 +1,12 @@
+import Items from "@/components/items"
+
+
+function ItemsPage() {
+  return (
+    <div className='pt-24'>
+      <Items />
+    </div>
+  )
+}
+
+export default ItemsPage
